2bfa68bc6756a97d63b3d25a15572a4ec4cb6902
ade Bello <belloade491@gmail.com>
2024-12-07 14:50:24 -0600

    newfiles added

---
+++ userinput.py
@@ -0,0 +1,7 @@
+print('inter your surname')
+surname=input()
+print('inter your firstname')
+firstname=input()
+print('inter your email')
+email=input()
+print('this are my details' ,surname ,firstname ,email)
