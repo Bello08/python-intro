cb8d5383df0760dcfd12269e0acf889430957dec
ade Bello <belloade491@gmail.com>
2025-02-09 13:18:02 -0600

    i did softwaredevelopment

---
+++ assignment1.py
@@ -0,0 +1,13 @@
+#mr john took a loan of principal 500k for two years at the r8 of 5%
+
+#write a python code  that will calculate the simple interest
+
+
+
+#Formula: SI=PTR/100
+
+p=500000
+t=2
+r=8
+si=p*t*r/100
+print(si)
